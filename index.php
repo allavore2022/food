470a99b26a97acf3c44d1c46c98327f58723ea2b
Alisa Llavore <allavore@mail.greenriver.edu>
2021-01-26 15:48:58 -0500

    Added more routes

--- index.php
+++ index.php
@@ -18,6 +18,12 @@ $f3->route('GET /', function () {
     echo $view->render('views/home.html');
 });
 
+//Define a default root (home page)
+$f3->route('GET /@first/@last', function ($f3, $params) {
+    echo "Hello, ".$params['first']." ".$params['last'];
+
+});
+
 //Define a "breakfast" route
 $f3->route('GET /breakfast', function () {
 //    echo "My Food Page";
@@ -30,6 +36,42 @@ $f3->route('GET /lunch', function () {
 //    echo "My Food Page";
     $view = new Template();
     echo $view->render('views/lunch.html');
+});
+
+//Define a "lunch/sandwich" route
+$f3->route('GET /breakfast/@item', function ($f3, $params) {
+    var_dump($params);
+    $menu = array('eggs', 'waffles', 'pancakes');
+    $item = $params['item'];
+    if (in_array($item, $menu)){
+        switch($item){
+            case 'eggs':
+                $view = new Template();
+                echo $view->render('views/eggs.html');
+                break;
+            case 'pancakes':
+                echo "Swedish or American?";
+                break;
+            case 'waffles':
+                $f3->rereoute("http://thewafflehouse.com");
+                break;
+            default:
+                $f3->error(404);
+        }
+    }
+    else {
+        echo "Sorry, we don't serve $item";
+    }
+//    echo "My Food Page";
+//    $view = new Template();
+//    echo $view->render('views/breakfast.html');
+});
+
+//Define a "lunch/sandwich" route
+$f3->route('GET /lunch/sandwich', function () {
+//    echo "My Food Page";
+    $view = new Template();
+    echo $view->render('views/sandwich.html');
 });
 
 //Rune fat free
